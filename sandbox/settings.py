1f00232aa16abd18109dfb222607bd4eb485eef0
NikolayKasatkin <6997620@gmail.com>
2022-11-20 18:12:03 +0300

    to local host

--- sandbox/settings.py
+++ sandbox/settings.py
@@ -125,18 +125,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-# STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, "static/")
-#
-# MEDIA_URL = '/media/'
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-#ДЛЯ ХОСТА********************************************
 STATIC_URL = '/static/'
-STATIC_ROOT = '/home/nftdesk/nft-desk/static'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = '/home/nftdesk/nft-desk/media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+#ДЛЯ ХОСТА********************************************
+# STATIC_URL = '/static/'
+# STATIC_ROOT = '/home/nftdesk/nft-desk/static'
+#
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = '/home/nftdesk/nft-desk/media'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
